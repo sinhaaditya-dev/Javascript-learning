be525649a4238e88bab7e03df7b0eff0dc56ee83
sinhaaditya-dev <sinhaaditya.2591@gmail.com>
2025-01-10 16:29:57 +0000

    variable in javascript

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,15 @@
 const account_id = 12345;
 let account_email ="abc@gmail.com";
-var account_password="122444";
-account_city="Jaipur";
+var account_password="122444";  //use let instead of var//
+account_city="Jaipur"; //This is not a good practice to declare a variable without let or var//
+let account_state; //Here we are declaring a variable without assigning a value//
 
 // account_id = 2;  we cannot change constant value which we declared account_id;//
 // console.log(account_id);
 account_email="sinhaaditya.2591@gmail.com";
 // console.log(account_email);
 account_city="Bengaluru";
-console.table([account_email,account_password,account_city]);  //here we can print the value of all the variables//
+console.table([account_id,account_email,account_password,account_city,account_state]);  //here we can print the value of all the variables//
+/*
+prefer not to use var
+ */
