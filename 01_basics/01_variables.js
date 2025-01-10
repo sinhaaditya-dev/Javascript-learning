cb63929c3dac4156e058ddc8473885536b1e28ea
sinhaaditya-dev <sinhaaditya.2591@gmail.com>
2025-01-10 16:13:32 +0000

    javacript config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const account_id = 12345;
+let account_email ="abc@gmail.com";
+var account_password="122444";
+account_city="Jaipur";
+
+// account_id = 2;  we cannot change constant value which we declared account_id;//
+// console.log(account_id);
+account_email="sinhaaditya.2591@gmail.com";
+// console.log(account_email);
+account_city="Bengaluru";
+console.table([account_email,account_password,account_city]);  //here we can print the value of all the variables//
