768cbe8c882d35bb7c146c36b4ec36183ca00145
sinhaaditya-dev <sinhaaditya.2591@gmail.com>
2025-01-11 06:28:57 +0000

    Datatpes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //Treat all js code as a newer version of js//
+
+// alert(3+3); // we are using node js browser, so we can't use alert//
+
+console.log(3+3);
+console.log("Aditya");
+
+let name= "Aditya";
+let age = 19;
+let isLoggedIn= false;
+
+//Datatypes list//
+//number =>2 to power 53
+//bigint ->for large value
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof "Aditya");  //It helps to find the datatype of element//
+console.log(typeof  null);
+console.log(typeof undefined);
